83e425b93710922be44f8e141f44ee803032806d
KelvinS elorm <131010014+Kelvinswebdev@users.noreply.github.com>
2023-12-29 08:12:57 +0000

    Update pay.js

--- js/pay.js
+++ js/pay.js
@@ -7,9 +7,9 @@ function payWithPaystack(e) {
   let handler = PaystackPop.setup({
     key: 'pk_live_5764b24e00d2d489b3dd51937033f86f4661d7c5', // Replace with your public key
     email: document.getElementById("email-address").value,
-    amount: document.getElementById("amount").value *200,
-    currency:'GHS',
-    ref: ''+Math.floor((Math.random() ), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
+    amount: document.getElementById("amount").value,
+    currency: 'GHS',
+    ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
     // label: "Optional string that replaces customer email"
     onClose: function(){
       alert('Window closed.');
