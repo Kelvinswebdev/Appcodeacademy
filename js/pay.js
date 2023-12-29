c054a39a4b79387d0244d567a58bc8d750111f07
KelvinS elorm <131010014+Kelvinswebdev@users.noreply.github.com>
2023-12-29 17:01:04 +0000

    Update pay.js

--- js/pay.js
+++ js/pay.js
@@ -1,24 +1,21 @@
-const paymentForm = document.getElementById('paymentForm');
-paymentForm.addEventListener("submit", payWithPaystack, false);
-
-function payWithPaystack(e) {
-  e.preventDefault();
-
-  let handler = PaystackPop.setup({
+var paymentForm = document.getElementById('paymentForm');
+paymentForm.addEventListener('submit', payWithPaystack, false);
+function payWithPaystack() {
+  var handler = PaystackPop.setup({
     key: 'pk_live_5764b24e00d2d489b3dd51937033f86f4661d7c5', // Replace with your public key
-    email: document.getElementById("email-address").value * 1000,
-    amount: document.getElementById("amount").value,
-    currency: 'GHS',
-    ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
-    // label: "Optional string that replaces customer email"
-    onClose: function(){
-      alert('Window closed.');
+    email: document.getElementById('email-address').value,
+    amount: document.getElementById('amount').value * 100, // the amount value is multiplied by 100 to convert to the lowest currency unit
+    currency: 'GHS', // Use GHS for Ghana Cedis or USD for US Dollars
+    ref: 'YOUR_REFERENCE', // Replace with a reference you generated
+    callback: function(response) {
+      //this happens after the payment is completed successfully
+      var reference = response.reference;
+      alert('Payment complete! Reference: ' + reference);
+      // Make an AJAX call to your server with the reference to verify the transaction
     },
-    callback: function(response){
-      let message = 'Payment complete! Reference: ' + response.reference;
-      alert(message);
-    }
+    onClose: function() {
+      alert('Transaction was not completed, window closed.');
+    },
   });
-
   handler.openIframe();
 }
