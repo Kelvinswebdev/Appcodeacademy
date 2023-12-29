2c3d11becccc598d6e1b5cde6261196cc51609cf
KelvinS elorm <131010014+Kelvinswebdev@users.noreply.github.com>
2023-12-29 08:02:00 +0000

    Update pay.js

--- js/pay.js
+++ js/pay.js
@@ -9,7 +9,7 @@ function payWithPaystack(e) {
     email: document.getElementById("email-address").value,
     amount: document.getElementById("amount").value *200,
     currency:'GHS',
-    ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
+    ref: ''+Math.floor((Math.random() ), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
     // label: "Optional string that replaces customer email"
     onClose: function(){
       alert('Window closed.');
